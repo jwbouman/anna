1b9c6c9293056161388afb4fa4e04151553ac57d
JWB <jan@compuguy.eu>
2026-08-11 14:53:59 +0200

    Initial Commit

---
+++ anna/annaApp.swift
@@ -0,0 +1,17 @@
+//
+//  annaApp.swift
+//  anna
+//
+//  Created by Jan Bouman on 11/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct annaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
